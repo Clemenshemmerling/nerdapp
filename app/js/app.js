34499a779c86132925c48e350323b5dd76b9fc27
Clemens Hemmerling <clemens@galileo.edu>
2017-07-19 23:15:05 -0600

    Agreagamos vue-resource y firebase al proyecto

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,25 @@
-$(document).foundation()
+var config = {
+    apiKey: "AIzaSyC4EQlOx8CtiNqOT85PVXL5g-1YHnxypFo",
+    authDomain: "nerdapp-252f5.firebaseapp.com",
+    databaseURL: "https://nerdapp-252f5.firebaseio.com",
+    projectId: "nerdapp-252f5",
+    storageBucket: "",
+    messagingSenderId: "942238883834"
+  };
+  firebase.initializeApp(config);
+
+  var app = new Vue({
+  el: '.container',
+  data: {
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  mounted: function () {
+    $(document).foundation()
+  }
+
+})
