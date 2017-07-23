46a7c770b8b122413d471d72ea58ee9b6d970de1
Clemens Hemmerling <clemens@galileo.edu>
2017-07-23 13:41:15 -0600

    Agregamos autentificacion

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,45 @@
 var config = {
-    apiKey: "AIzaSyC4EQlOx8CtiNqOT85PVXL5g-1YHnxypFo",
-    authDomain: "nerdapp-252f5.firebaseapp.com",
-    databaseURL: "https://nerdapp-252f5.firebaseio.com",
-    projectId: "nerdapp-252f5",
-    storageBucket: "",
-    messagingSenderId: "942238883834"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyC4EQlOx8CtiNqOT85PVXL5g-1YHnxypFo",
+  authDomain: "nerdapp-252f5.firebaseapp.com",
+  databaseURL: "https://nerdapp-252f5.firebaseio.com",
+  projectId: "nerdapp-252f5",
+  storageBucket: "nerdapp-252f5.appspot.com",
+  messagingSenderId: "942238883834"
+};
+firebase.initializeApp(config);
+
+var db = firebase.database(),
+    auth = firebase.auth(),
+    provider = new firebase.auth.GoogleAuthProvider()
 
   var app = new Vue({
   el: '.container',
   data: {
+    autentificado: false,
+    activeUser : null
   },
   computed: {
 
   },
   methods: {
-
+    login: function () {
+      // obtener email y password
+      const email = $('#email').val()
+      const pass = $('#passwd').val()
+      const auth = firebase.auth()
+      // registrarse
+      const promise = auth.signInWithEmailAndPassword(email, pass)
+      promise.catch(e => console.log(e.message))
+    }
   },
   mounted: function () {
     $(document).foundation()
-  }
-
+    auth.onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user);
+      } else {
+        console.warn('no conectado');
+      }
+    });
+  },
 })
